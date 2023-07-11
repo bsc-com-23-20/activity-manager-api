1ca013b4b954586c6f27d95a6bc6a55f34092422
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-07-11 17:22:34 +0200

    few changes

--- src/Users/user.module.ts
+++ src/Users/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database.provider';
 import { userProviders } from 'src/providers/users.provider';
 import { UsersService } from './user.service';
+import { UsersController } from './user.controller';
 
 @Module({
   imports: [DatabaseModule],
@@ -9,5 +10,7 @@ import { UsersService } from './user.service';
     ...userProviders,
     UsersService,
   ],
+  controllers: [UsersController]
+  
 })
 export class UsersModule {}
