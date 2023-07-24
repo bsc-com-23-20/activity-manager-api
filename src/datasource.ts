4456a3a9f9fd8e46fa4f32630aab7a22989100db
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-07-24 19:36:34 +0200

    hosted the database online

--- src/datasource.ts
+++ src/datasource.ts
@@ -8,11 +8,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: 'sql7.freemysqlhosting.net',
         port: 3306,
-        username: 'root',
-        password: '',
-        database: 'activitymanager',
+        username: 'sql7634974',
+        password: 'pelp74SSKf',
+        database: 'sql7634974',
         entities: [
             Task,User
         ],
