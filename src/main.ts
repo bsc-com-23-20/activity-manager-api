9dc9d51c6335e4c48333df7cf02633ae5c313c90
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-07-24 20:16:24 +0200

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,8 +14,8 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
